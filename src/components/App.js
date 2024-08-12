ca31645e9a4195d4a62734fd4e246ac568744620
Ndegwa2 <138137867+Ndegwa2@users.noreply.github.com>
2024-08-12 15:40:30 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import blogData from "../data/blog";
-
-console.log(blogData);
+import Header from "./Header";
+import About from "./About";
+import ArticleList from "./ArticleList";
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header name="My Bloggy Blog" /> {/*  blog name */}
+      <About 
+        image="https://via.placeholder.com/215" 
+        about="This is a personal blog where I share my thoughts." // blog description
+      />
+      <ArticleList articles={blogData} />
     </div>
   );
 }
